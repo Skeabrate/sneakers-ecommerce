62bcc1d6628cb464ed5e0ff61c76de335dd01d9f
unknown <sebastianswiecz458@gmail.com>
2021-11-27 04:32:17 +0100

    fixed filter list borders

--- src/Components/ComboBox/ComboBox.styles.js
+++ src/Components/ComboBox/ComboBox.styles.js
@@ -7,17 +7,14 @@ export const Wrapper = styled.div`
 
 export const StyledOptionBtn = styled.button`
    text-transform: uppercase;
-   border: none;
    background-color: ${({isOpen, theme}) => isOpen ? `${theme.colors.black}` : 'transparent'};
    color: ${({theme}) => theme.colors.white}; 
    font-size: ${({theme}) => theme.fontSize.s};
-   position: relative;
    border: 1px solid ${({isOpen, theme}) => isOpen ? theme.colors.orange : 'transparent'};
    border-bottom: ${({isOpen}) => isOpen ? `none` : null};
    height: 40px;
    width: fit-content;
    padding: 10px 30px 10px 12px;
-   text-align: left;
 
    span{
       position: absolute;
@@ -45,12 +42,21 @@ export const StyledList = styled.ul`
    scroll-behavior: smooth;
    right: 0;
    left: ${({isPrice}) => isPrice ? 'unset' : '0'};
-   top: 99%;
+   top: 39px;
    border: 1px solid ${({theme}) => theme.colors.orange};
    background-color: ${({theme}) => theme.colors.black};
    z-index: -1;
    visibility: ${({isOpen}) => isOpen ? 'visible' : 'hidden'};
    box-shadow: 0px 0px 26px -11px rgba(0, 0, 0, 1);
+
+   li{
+      padding: 20px 15px;
+   }
+
+   &:focus{
+      outline: none;
+      border: 1px solid ${({theme}) => theme.colors.orange};
+   }
 
    ::-webkit-scrollbar {
       width: 8px;
@@ -63,14 +69,5 @@ export const StyledList = styled.ul`
    ::-webkit-scrollbar-thumb {
       background: linear-gradient(180deg, #D0368A 0%, ${({theme}) => theme.colors.orange} 99%);
       border-radius: 100px;
-   }
-
-   li{
-      padding: 20px 15px;
-   }
-
-   &:focus{
-      outline: none;
-      border: 1px solid ${({theme}) => theme.colors.orange};
    }
 `
