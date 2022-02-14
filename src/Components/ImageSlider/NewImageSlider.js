0f3c35eda537ef5f2c416e9ad02890fdea160744
unknown <sebastianswiecz458@gmail.com>
2022-02-14 15:53:27 +0100

    Added animation for image slider

--- src/Components/ImageSlider/NewImageSlider.js
+++ src/Components/ImageSlider/NewImageSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import {
@@ -8,23 +8,49 @@ import {
     StyledBtnPrev,
     StyledBtnNext,
 } from "./NewImageSlider.styles"
+import { imgLoad } from '../../helpers/imgLoad';
+import gsap from "gsap"
 
 const NewImageSlider = ({ product, loading }) => {
+    const tl = useRef(null)
+    const imgRef = useRef(null)
+    const imgContRef = useRef(null)
+
     const props = {
         autoplay: false,
         transitionDuration: 400,
         indicators: true,
         prevArrow: <StyledBtnPrev><svg width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/></svg></StyledBtnPrev>,
-        nextArrow: <StyledBtnNext><svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/></svg></StyledBtnNext>
+        nextArrow: <StyledBtnNext><svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" strokeWidth="3" fill="none" fillRule="evenodd"/></svg></StyledBtnNext>,
     }
+
+    useEffect(() => {
+        var image = document.createElement('img')
+        if(loading && imgRef.current){
+            image.src = imgLoad(imgRef.current)
+            image.onload = function() {
+                tl.current.play() 
+            }
+        }
+        
+        tl.current = gsap.timeline({ paused: true })
+        if(tl.current) {
+            tl.current
+                .to(imgContRef.current, {
+                    opacity: 1,
+                    duration: .8,
+                })
+        }
+        
+    }, [loading])
 
     return (
         <Wrapper>
-            <SlideContainer>
+            <SlideContainer ref={imgContRef}>
                 <Slide {...props}>
                     {loading && product.images.map(({ url }, index) => (
                         <div key={index}>
-                            <StyledSlide url={url} />
+                            <StyledSlide url={url} ref={!index ? imgRef : null} />
                         </div>
                     ))} 
                 </Slide>
--- src/Components/ImageSlider/NewImageSlider.styles.js
+++ src/Components/ImageSlider/NewImageSlider.styles.js
@@ -25,6 +25,7 @@ export const SlideContainer = styled.div`
     top: 0;
     width: 100%;
     height: 100%;
+    opacity: 0;
 `
 
 export const StyledSlide = styled.div`
