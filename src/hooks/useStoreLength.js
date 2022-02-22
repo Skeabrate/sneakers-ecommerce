5c31822945bf81c0bedb69f465a81ffb7c57316b
unknown <sebastianswiecz458@gmail.com>
2022-02-22 13:41:58 +0100

    useStoreLength - fixed

--- src/hooks/useStoreLength.js
+++ src/hooks/useStoreLength.js
@@ -4,8 +4,7 @@ export const useStoreLength = (store = []) => {
    const [length, setLength] = useState(0)
 
    useEffect(() => {
-      const data = store.map(item => item.amount)
-      setLength(data.reduce((a, b) => a + b, 0))
+      setLength(store.reduce((a, b) => a + b.amount, 0))
    }, [store])
 
    return length > 0 ? length : 0;
