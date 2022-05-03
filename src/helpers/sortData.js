d5df047b557ea295f3a01ae70b064a39509d9861
unknown <sebastianswiecz458@gmail.com>
2022-05-03 18:54:20 +0200

    SortFunc refactored

--- src/helpers/sortData.js
+++ src/helpers/sortData.js
@@ -1,15 +1,4 @@
 export const sortData = (data, option) => {
-	if (option === 'ascending') {
-		return data.sort((a, b) => {
-			if (a.price < b.price) return -1;
-			if (a.price > b.price) return 1;
-			return 0;
-		});
-	} else if (option === 'descending') {
-		return data.sort((a, b) => {
-			if (a.price > b.price) return -1;
-			if (a.price < b.price) return 1;
-			return 0;
-		});
-	}
+	if (option === 'ascending') data.sort((a, b) => a.price - b.price);
+	else if (option === 'descending') data.sort((a, b) => b.price - a.price);
 };
